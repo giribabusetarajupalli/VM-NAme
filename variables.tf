ec1d9bcd16ab38677ec708c6fe5bbfd6544ec5cc
giribabusetarajupalli <79827252+giribabusetarajupalli@users.noreply.github.com>
2021-03-01 12:57:05 +0530

    vm creation

--- variables.tf
+++ variables.tf
@@ -1,12 +1,31 @@
-variable "datadisk" {
-  type = object({
-    a = string
-    b = string
-    c = string
-  })
-  default = {
-    a            : "testdisk"
-    b            : "newdisk"
-    c            : "olddisk"
-  }
+variable "rg_name" {
+    type = string
+}
+
+variable "vnet_name" {
+    type = string
+}
+
+variable "subnet_name" {
+    type = string
+}
+
+variable "vnet_address_space" {
+    type = list(string)
+}
+
+variable "subnet_address_space" {
+    type = string
+}
+
+variable "nic_name" {
+    type = string
+}
+
+variable "vm_name" {
+    type = string
+}
+
+variable "vm_size" {
+    type = string
 }
